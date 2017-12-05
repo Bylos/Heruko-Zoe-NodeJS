a546b49f9d549a80f5db9a30837509a6f82f3903
Bertrand Massot <bertrand.massot@insa-lyon.fr>
2017-12-05 20:42:27 +0100

    Fix missing ')' after argument list

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ wss.on('connection', function connection(ws, req) {
 	
 	ws.on('message', function incoming(message) {
 		console.log('received: %s', message);
-	};
+	});
 	
 	ws.send('something');
 });
