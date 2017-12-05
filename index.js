4b81b5ce77dbf87ab0882e76b51175f4bc3ff6c5
Bertrand Massot <bertrand.massot@insa-lyon.fr>
2017-12-05 20:39:46 +0100

    Implement http and websocket

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5687
+const http = require('http');
+const url = require('url');
+const WebSocket = require('ws');
 
-const app = express()
+const PORT = process.env.PORT || 5687
+const app = express();
 
 app.post('/', function (req, res) {
-	res.json({'speech': 'bla bla bla', 'displayText': 'bla bla bla'});
+	res.json({ 'speech': 'bla bla bla', 'displayText': 'bla bla bla' });
 	console.log('We did have a request');
-})
+});
 
-app.listen(PORT, function () {
-	console.log('Example app listening on port zz!');
-})
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', function connection(ws, req) {
+	const location = url.parse(req.url, true);
+	
+	ws.on('message', function incoming(message) {
+		console.log('received: %s', message);
+	};
+	
+	ws.send('something');
+});
+
+server.listen(PORT, function listening() {
+	console.log('Listening on %d', server.address().port);
+});
